422a4260ab11644e73c8b4ce7662b4fb5cf82220
David Freedman <davidjfreedman@gmail.com>
2014-10-21 20:24:19 -0500

    site redo

--- app/app.js
+++ app/app.js
@@ -1,152 +1,44 @@
 function app() {
-
-
-
-    //SPLASH SCREEN
-
-
-    var projArraySpot = 0;
-
-    $(".iconBox").hover(
-        function() {
-            var hoverClassName = $(this).attr('class');
-            // var hoverOutput = '';
-            // if (hoverClassName.indexOf('iconBox') !== -1) {
-            //     hoverOutput = '.linkDescription';
-            // };
-            if (hoverClassName === 'call iconBox') {
-                $('.linkDescription')[0].textContent = 'call me';
-            } else if (hoverClassName === 'email iconBox') {
-                $('.linkDescription')[0].textContent = 'email me';
-            } else if (hoverClassName === 'download iconBox') {
-                $('.linkDescription')[0].textContent = 'download my resume';
-            } else if (hoverClassName === 'github iconBox') {
-                $('.linkDescription')[0].textContent = 'check out my github';
-            } else if (hoverClassName === 'linkedin iconBox') {
-                $('.linkDescription')[0].textContent = 'view my linkedin';
-            };
-            // if $( this) ===
-        },
-        function() {
-            $('.linkDescription')[0].textContent = '- - -';
-        });
-    $('.goto').hover(
-        function() {
-            if ($(this).attr('class') !== "projectsButton goto") {
-                $(this)[0].textContent = "Coming Soon";
-            }
-        },
-        function() {
-            var hoverClassName = $(this).attr('class');
-            if (hoverClassName === 'projectsButton goto') {
-                $(this)[0].textContent = "Projects";
-            } else if (hoverClassName === 'experienceButton goto') {
-                $(this)[0].textContent = "Experience";
-            } else if (hoverClassName === 'aboutButton goto') {
-                $(this)[0].textContent = "About";
-            };
-        });
-
-
-
-
-    //PROJECTS SECTION
-
-
-
-
-    var projectsArray = [{
-        name: "Affluentsy",
-        image: "affluentsy.jpg",
-        description: "Designed as a tongue-in-cheek blend of high-end retail sites and a the more hands-on Etsy, this project incorporates search engines, categorizing, sorting, and more.",
-        liveLink: "http://shielded-lake-4499.herokuapp.com",
-        gitSite: "https://github.com/davidjfreedman/FinalProject",
-        tech: "jQuery, Javascript, Myth, lodash/underscore templating, FontAwesome, Photoshop, JS Prototypes, Node.js, Etsy API, heroku, HTML, CSS, CSS3"
-    }, {
-        name: "Color Clock",
-        image: "colorclock.jpg",
-        description: "It started as a clock where the time values shift the background color. It soon became... odder.",
-        liveLink: "http://davidjfreedman.github.io/HW23-Dates/",
-        gitSite: "https://github.com/davidjfreedman/HW23-Dates",
-        tech: "Javascript, jQuery, HTML, CSS"
-    }, {
-        name: "Wat Watch",
-        image: "watwatch.jpg",
-        description: "A simple, normal stopwatch app.<br> Hmm. we'll have to do something about that 'normal' bit soon...",
-        liveLink: "http://davidjfreedman.github.io/HW25-watwatch/",
-        gitSite: "https://github.com/davidjfreedman/HW25-watwatch",
-        tech: "Javascript, jQuery, HTML, CSS"
-    }, ]
-    for (var i = 1; i < projectsArray.length; i++) {
-        var oneProj = projectsArray[i];
-        var projHTML = '<div class="projectBox secondaryProjBoxes"><div class="projTopSection"><img class="projImage" src="../assets/images/projects/' + oneProj.image + '" /></div><div class="projBottomSection"><div class="projTitle">' + oneProj.name + '</div><div class="projDescription">' + oneProj.description + '</div><div class="projLinks"><a href="' + oneProj.liveLink + '"><div class="liveProjLink">LIVE SITE</div></a><a href="' + oneProj.gitSite + '"><div class="gitProjLink">CODE</div></a></div><div class="projTech"><p class="techTitle">Tech Used</p><p class="techText">' + oneProj.tech + '</p></div></div></div>';
-        $('.projectsContent').append(projHTML);
-    };
-
-    $('.outerProjPortions').on('click', function() {
-        if ($(this).attr('class') === "outerProjPortions leftProjNav") {
-            projArraySpot--;
-            if (projArraySpot < 0) {
-                projArraySpot = projectsArray.length - 1;
-            };
-        } else {
-            projArraySpot++;
-            if (projArraySpot === projectsArray.length) {
-                projArraySpot = 0;
-            };
-        }
-        var projHTML = '<div class="projTopSection"><img class="projImage" src="../assets/images/projects/' + projectsArray[projArraySpot].image + '" /></div><div class="projBottomSection"><div class="projTitle">' + projectsArray[projArraySpot].name + '</div><div class="projDescription">' + projectsArray[projArraySpot].description + '</div><div class="projLinks"><a href="' + projectsArray[projArraySpot].liveLink + '"><div class="liveProjLink">LIVE SITE</div></a><a href="' + projectsArray[projArraySpot].gitSite + '"><div class="gitProjLink">CODE</div></a></div><div class="projTech"><p class="techTitle">Tech Used</p><p class="techText">' + projectsArray[projArraySpot].tech + '</p></div></div>';
-        $('.mainProjBox')[0].innerHTML = projHTML;
-
-    });
-    // each object is the code inset for the projects page
-    // on
-
-    //HEADER ON/OFF
-
-    $(window).scroll(function() {
-        if (window.scrollY >= $('.splashScreen')[0].offsetHeight) {
-            $('.header').addClass('headerOn');
-        } else {
-            $('.header').removeClass('headerOn');
-            $('.menuOpen').removeClass('menuOpenOn');
-        }
-    });
-
-    if (window.scrollY >= $('.splashScreen')[0].offsetHeight) {
-        $('.header').addClass('headerOn');
-    }
-
     //OPEN/CLOSE MENU
     $('.menuButton').on('click', function() {
-        $('.leftMenu').toggleClass('menuOpenOn');
-    });
-
-    $('.headerOutsideLinks').on('click', function() {
         $('.rightMenu').toggleClass('menuOpenOn');
     });
 
-    $('.mask').click(function() {
+    $('.body').click(function() {
         if ($('.menuOpen').hasClass('menuOpenOn')) {
-
-            $('.leftMenu').removeClass('menuOpenOn');
-            $('.rightMenu').removeClass('menuOpenOn');
+            $('.menuOpen').removeClass('menuOpenOn');
         };
     }); // clicking outside the open menu closes it.
 
+    //CLOSE MENU WHEN CLICK LINK
+    $('.menuLink').on('click', function() {
+        $('.menuOpen').removeClass('menuOpenOn');
+    })
+
     $(window).resize(function() {
         if ($('.header').width() >= 900) {
-            $('.leftMenu').removeClass('menuOpenOn');
+            $('.menuOpen').removeClass('menuOpenOn');
         }
     }); // closes menu if window wide enough for topnav
 
-    $('body').on('click', '.menuLink .topButton .headerLink', function() {
-        $("html, body").animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 625);
-    });
 
+    //OPEN/CLOSE JOB INFOBOXES
+    $('.jobInfo').on('click', function() {
+        //save class info to var
+        //if var already open, closes var
+        //if not:
+        //removes 'on' class from all
+        //adds 'on' to var
+        var clickedClass = this.className;
+        console.log(clickedClass)
+        if ($(this).hasClass('jobInfoShowing')) {
+            $(this).removeClass('jobInfoShowing');
+        } else {
+            $('.jobInfo').removeClass('jobInfoShowing');
+            $(this).addClass('jobInfoShowing');
+        }
 
+    })
 }
 
-window.onload = app;
+window.onload = app();
